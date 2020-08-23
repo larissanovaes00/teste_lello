53e56aafa7cdaa429999cb28c732d6f1bd24f596
Larissa Novaes <larissanovaes00@hotmail.com>
2020-08-23 15:29:29 -0300

    implementing initial template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { SearchComponent } from './components/search/search.component';
+import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { HomeContainerComponent } from './components/home-container/home-container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SidemenuComponent,
+    UsersListComponent,
+    ModalComponent,
+    HomeContainerComponent
   ],
   imports: [
     BrowserModule,
